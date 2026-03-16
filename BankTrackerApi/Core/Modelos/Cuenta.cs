6a97b5cc2dd547d86e589fb46a32a08820588984
Miguel Diaz <miguel.diazv84@gmail.com>
2026-03-16 13:51:39 +0100

    Implementa modelos base y conexión Dapper a MySQL
    Se agregan las clases Cuenta y Movimientos para modelar cuentas bancarias y sus movimientos, así como el enum TipoMovimiento. Se configura la cadena de conexión a MySQL en appsettings.json y se añade DapperContext para gestionar conexiones a la base de datos.

---
+++ BankTrackerApi/Core/Modelos/Cuenta.cs
@@ -0,0 +1,13 @@
+﻿namespace BankTrackerApi.Core.Modelos
+{
+    public class Cuenta
+    {
+        private int Id { get; set; }
+        private string NumeroCuenta { get; set; }
+        private string NombreTitular { get; set; }
+        private decimal Saldo { get; set; }
+        private string PasswrodHash { get; set; }
+        private List<Movimientos> Movimientos { get; set; }
+
+    }
+}
---
+++ BankTrackerApi/Core/Modelos/Movimientos.cs
@@ -0,0 +1,12 @@
+﻿namespace BankTrackerApi.Core.Modelos
+{
+    public class Movimientos
+    {
+        private int Id { get; set; }
+        private int CuentaId { get; set; }
+        private decimal Cantidad { get; set; }
+        private string Concepto { get; set; }
+        private DateTime Fecha { get; set; }
+        private string TipoMovimiento { get; set; }
+    }
+}
