a6500f47b34567d930c6a00a9ba1d618bc2fc987
Miguel Diaz <miguel.diazv84@gmail.com>
2026-03-17 10:03:46 +0100

    Añadir proyecto compartido y modelos de datos
    Se ha añadido un nuevo proyecto `BankTrackerShared` al archivo de solución `BankTrackerApi.slnx` para centralizar componentes compartidos. Se han actualizado `BankTrackerApi.csproj` y `BankTrackerApp.Shared.csproj` para referenciar este nuevo proyecto. En `BankTrackerApp.csproj`, se han eliminado los frameworks de destino para iOS y MacCatalyst, enfocándose solo en Android. Se han introducido varias clases en `BankTrackerShared` para modelar cuentas bancarias, movimientos y DTOs para solicitudes y respuestas de API.

---
+++ BankTrackerShared/Core/Modelos/Cuenta.cs
@@ -0,0 +1,14 @@
+﻿namespace BankTrackerShared.Core.Modelos
+{
+    public class Cuenta
+    {
+        public int Id { get; set; }
+        public string NumeroCuenta { get; set; }
+        public string NombreTitular { get; set; }
+        public string DniCliente { get; set; }
+        public decimal Saldo { get; set; }
+        public string PasswordHash { get; set; }
+        public List<Movimientos> Movimientos { get; set; }
+
+    }
+}
---
+++ BankTrackerShared/Shared/DTOs/LoginRequest.cs
@@ -0,0 +1,8 @@
+namespace BankTrackerShared.Shared.DTOs
+{
+    public class LoginRequest
+    {
+        public string DniCliente { get; set; }
+        public string Password { get; set; }
+    }
+}
