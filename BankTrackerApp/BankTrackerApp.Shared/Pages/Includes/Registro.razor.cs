7006843443f4dea77e10bc518a2fd5ba348299b6
Miguel Diaz <miguel.diazv84@gmail.com>
2026-03-18 09:42:07 +0100

    Añadir diseño y funcionalidad de registro
    Se ha creado un nuevo componente de diseño `LoginLayout` en `LoginLayout.razor` utilizando `MudLayout` para estructurar la página de inicio de sesión. En `Login.razor`, se ha integrado este diseño y se ha añadido un enlace para el registro de nuevos usuarios. La lógica de inicio de sesión se ha movido a `Login.razor.cs`.
    
    Se ha añadido una nueva página `Registro.razor` con un formulario para crear cuentas, y su lógica correspondiente se encuentra en `Registro.razor.cs`. Además, se ha actualizado `_Imports.razor` para incluir `BankTrackerApp.Shared.Layout`.

---
+++ BankTrackerApp/BankTrackerApp.Shared/Pages/Includes/Registro.razor.cs
@@ -0,0 +1,40 @@
+﻿using BankTrackerShared.Shared.DTOs;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTrackerApp.Shared.Pages.Includes
+{
+    public partial class Registro
+    {
+        private CuentaRequest crearCuenta = new();
+
+        private async Task CrearCuenta()
+        {
+            try
+            {
+                var response = await Http.PostAsJsonAsync("/api/cuentas/register", crearCuenta);
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var result = response.Content.ReadFromJsonAsync<ApiResponse<CuentaResponse>>();
+
+                    if (result != null)
+                    {
+                        await JS.InvokeVoidAsync("alert", "Usuario creado exitosamente. Ahora puedes iniciar sesión.");
+                        Navigation.NavigateTo("/login");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await JS.InvokeVoidAsync("alert", $"Ocurrió un error: {ex.Message}");
+            }
+        }
+    }
+}
